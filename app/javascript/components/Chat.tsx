5f757f65c8fc120934107893ac2eae75a4a31a46
Herrick Fang <herrickfang@gmail.com>
2023-03-21 23:29:19 -0700

    add nice chat

--- app/javascript/components/Chat.tsx
+++ app/javascript/components/Chat.tsx
@@ -1,7 +1,8 @@
+import { observer } from "mobx-react";
 import React, { useEffect } from "react";
 import { useRootStore } from "../hooks/useRootStore";
 
-export const Chat = () => {
+export const Chat = observer(() => {
   const rootStore = useRootStore();
 
   useEffect(() => {
@@ -10,18 +11,28 @@ export const Chat = () => {
 
   return (
     <div>
+      <span>Messages</span>
+      {rootStore.chatStore.chat.messages.map((text) => {
+        return <span>{text}</span>;
+      })}
+      <span>{rootStore.chatStore.chat.currentStream}</span>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          rootStore.chatStore.sendMessage("hello");
+          const message = e.target[0].value;
+          if (rootStore.chatStore.isSending || message === "") {
+            return false;
+          } else {
+            rootStore.chatStore.sendMessage(message);
+          }
         }}
       >
         <label>
           Message
           <input name="message"></input>
         </label>
-        <button>Chat</button>
+        <button disabled={rootStore.chatStore.isSending}>Chat</button>
       </form>
     </div>
   );
-};
+});
--- app/javascript/stores/ChatStore.ts
+++ app/javascript/stores/ChatStore.ts
@@ -2,29 +2,56 @@ import { makeAutoObservable } from "mobx";
 import { RootStore } from "./RootStore";
 import { createConsumer, Consumer } from "@rails/actioncable";
 
+class Chat {
+  messages: Array<string> = [];
+  currentStream: string = "";
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  addToStream(text: string) {
+    this.currentStream += text;
+  }
+
+  finishStream() {
+    this.messages.push(this.currentStream);
+    this.currentStream = "";
+  }
+}
+
 export class ChatStore {
   rootStore: RootStore;
   consumer: Consumer | null = null;
+  chat: Chat;
+  isSending: boolean;
 
   constructor(rootStore: RootStore) {
     makeAutoObservable(this);
     this.rootStore = rootStore;
+    this.chat = new Chat();
   }
 
   initWebsocket() {
-    console.log("once");
     this.consumer = createConsumer("/cable");
     this.consumer.subscriptions.create(
       { channel: "UserChannel" },
       {
-        received(data) {
+        received: (data) => {
           console.log(data);
+          if (data["output"]) {
+            this.chat.addToStream(data["output"]);
+          }
+          if (data["done"] === true) {
+            this.chat.finishStream();
+          }
         },
       }
     );
   }
 
   async sendMessage(message, chatId = null) {
+    this.isSending = true;
     const result = await fetch("/api/chat", {
       method: "POST",
       headers: { "content-type": "application/json" },
@@ -34,5 +61,6 @@ export class ChatStore {
         [this.rootStore.csrfParams.csrfParam]: this.rootStore.csrfParams.csrfToken,
       }),
     });
+    this.isSending = false;
   }
 }
